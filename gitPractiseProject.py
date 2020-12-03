75ecc7db3c6c1ceee5980ea8c25f0ac5fb1da153
Rita Cong <congrita80@gmail.com>
2020-12-03 10:12:53 -0800

    Add gitPractiseProject.py to repo

---
+++ gitPractiseProject.py
@@ -0,0 +1,13 @@
+# GitPractiseProject
+
+#PractiseProject to demonstrate git workflow
+
+def main():
+    #find the nth Fibonacci number
+    #eg  1 1 2 3 5 8 13 21 34 55
+    n = int(input("what fnumber do you want?" ))
+    if n in [1,2]:
+       print("The first fnumber ")
+
+if __name__ == "__main__":
+    main()
