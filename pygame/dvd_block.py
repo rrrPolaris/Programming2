64e5142d5092dccd58aec5208f76d164984d009b
Rita Cong <congrita80@gmail.com>
2021-01-14 11:06:44 -0800

    Updated multiple files

--- pygame/dvd_block.py
+++ pygame/dvd_block.py
@@ -206,3 +206,5 @@ def main():
         clock.tick(60)
 
     pygame.quit()
+    if __name__ == "__main__":
+        main()
---
+++ webscraping/map.py
@@ -0,0 +1,20 @@
+
+# mapit.py - tool to search physical addresses
+# usage -- python mapit.py 6600 Williams Road Richmond
+#     output: should take us to the address in GMaps
+
+import webbrowser
+import sys
+import pyperclip
+
+
+# if there are NO ARGUMENTS
+# grab address from the clipboard
+if len(sys.argv) < 2:
+    address = pyperclip.paste()
+else:
+    address = " ".join(sys.argv[1:])
+
+print(f"Finding {address}...")
+prefix = "https://google.com/maps/place/"
+webbrowser.open(prefix+address)
